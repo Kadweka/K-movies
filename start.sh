bb2e93adfd31ba31bf76d785127ed83afff0eae9
Hussein Kadweka <developersjuniors@gmail.com>
2019-10-30 17:12:51 +0300

    defined a start.sh to the application

---
+++ start.sh
@@ -0,0 +1,9 @@
+ 
+export MOVIE_API_KEY='6b2931ce73d6552995776353db108a22'
+export SECRET_KEY='123456'
+export MAIL_USERNAME="developersjuniors@gmail.com"
+export MAIL_PASSWORD="Nairobi001"
+export DATABASE_URL='postgresql+psycopg2://moringa:qwerty12@localhost/movies'
+
+
+python3.6 manage.py server
