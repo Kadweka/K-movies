e46f03f9bd35bb0ba8aa91f53e36a61c75b7ff72
Hussein Kadweka <developersjuniors@gmail.com>
2019-10-30 18:24:36 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,45 +2,56 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-
+   '''
+   General configuration parent class
+   '''
+   pass
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:qwerty12@localhost/movies'
+   MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+   MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'K-movies'
-    MAIL_USERNAME="developersjuniors@gmail.com"
-    MAIL_PASSWORD="Nairobi001"
+   MAIL_SERVER = 'smtp.googlemail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = True
+   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+   UPLOADED_PHOTOS_DEST = "app/static/photos"
+   SUBJECT_PREFIX = 'K-movies'
+   MAIL_USERNAME="developersjuniors@gmail.com"
+   MAIL_PASSWORD="Nairobi001"
+   SECRET_KEY = os.environ.get('SECRET_KEY')
+   SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-    @staticmethod
-    def init_app(app):
+   SIMPLEMDE_JS_IIFE = True
+   SIMPLEMDE_USE_CDN = True
+   @staticmethod
+   def init_app(app):
         pass
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
-
+   '''
+   Production  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   pass
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty12@localhost/movies_test'
-
+    SQLALCHEMY_DATABASE_URL='postgresql+psycopg2://moringa:qwerty12@localhost/movies'
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty12@localhost/movies'
-    DEBUG = True
+   '''
+   Development  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATDATABASE_URL='postgresql+psycopg2://moringa:qwerty12@localhost/movies'
+   DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
 'test':TestConfig
-
 }
--- start.sh
+++ start.sh
@@ -4,6 +4,4 @@ export SECRET_KEY='123456'
 export MAIL_USERNAME="developersjuniors@gmail.com"
 export MAIL_PASSWORD="Nairobi001"
 export DATABASE_URL='postgresql+psycopg2://moringa:qwerty12@localhost/movies'
-
-
 python3.6 manage.py server
